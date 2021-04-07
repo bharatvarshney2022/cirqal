75f2cf31b8b377e6c0393b724cce77f0ba2e127f
Munjal Mayank <munjaldevelopment@gmail.com>
2021-04-07 20:09:26 +0530

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -20,9 +20,11 @@ class HomeController extends Controller
 		$categoryData = ArticleCategory::whereNull('parent_id')->where('depth', '1')->orderBy('lft', 'ASC')->get();
 		
 
-		$articleData = \DB::table('news_articles')->leftJoin('users', 'news_articles.author_id', '=', 'users.id')->where('featured', '0')->where('status', 'PUBLISHED')->selectRaw('news_articles.*,users.name,users.user_photo')->get();
+		$articleNonFeaturedData = \DB::table('news_articles')->leftJoin('users', 'news_articles.author_id', '=', 'users.id')->where('featured', '0')->where('news_articles.status', 'PUBLISHED')->selectRaw('news_articles.*,users.name,users.user_photo')->get();
 
-		return view('article-listing', ['title' => $pageData->meta_title, 'meta_description' => $pageData->meta_description, 'meta_keywords' => $pageData->meta_keywords, 'categoryData' => $categoryData, 'articleData' => $articleData]);
+		$articleFeaturedData = \DB::table('news_articles')->leftJoin('users', 'news_articles.author_id', '=', 'users.id')->where('featured', '1')->where('news_articles.status', 'PUBLISHED')->selectRaw('news_articles.*,users.name,users.user_photo')->get();
+
+		return view('article-listing', ['title' => $pageData->meta_title, 'meta_description' => $pageData->meta_description, 'meta_keywords' => $pageData->meta_keywords, 'categoryData' => $categoryData, 'articleData' => $articleNonFeaturedData, 'articleFeaturedData' => $articleFeaturedData]);
 
 		return view('');
 	}
